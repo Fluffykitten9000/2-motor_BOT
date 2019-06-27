ade14cb9cc6f6c7d9d1954a5fc6ec503f14c07e9
Fluffykitten9000 <50753141+Fluffykitten9000@users.noreply.github.com>
2019-06-27 16:42:33 -0400

    Update autoIMU.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoIMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoIMU.java
@@ -139,8 +139,10 @@ public class autoIMU extends LinearOpMode {
         brd.setPower(0);
     }
     
-    Public void gyroTurn(double speed,double angle,double time) {
-        While (opModeIsActive()&&runtime.milliseconds()<=time
+    public void gyroTurn(double speed,double angle,double time) {
+        while (opModeIsActive()&&runtime.milliseconds()<=time) {
+
+        }
     }
 
     // both of these are for help with the accelerometer
