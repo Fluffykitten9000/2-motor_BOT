986309e8a1693056e599028682e46b0d1bd4270e
Fluffykitten9000 <50753141+Fluffykitten9000@users.noreply.github.com>
2019-06-27 13:52:24 -0400

    Update autoIMU.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoIMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoIMU.java
@@ -1,31 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 package org.firstinspires.ftc.teamcode;
 
@@ -54,12 +26,12 @@ import java.util.Timer;
 @Autonomous(name="IMU test", group="auto with IMU")
 public class autoIMU extends LinearOpMode {
 
-    /* Declare OpMode members. */
-
+    //the references to the motors
     private DcMotor fld = null;
     private DcMotor frd = null;
     private DcMotor bld = null;
     private DcMotor brd = null;
+    //the accelerometer calibrate
     private double acc[] = new double[3];
     private double accOff[] = new double[3];
 
@@ -139,6 +111,11 @@ public class autoIMU extends LinearOpMode {
             bld.setPower(Range.clip(speed + Range.scale(angles.thirdAngle, -MAX_SCALE_ANGLE, MAX_SCALE_ANGLE, -SCALED_NUM, SCALED_NUM), -1, 1));
             brd.setPower(Range.clip(speed + -Range.scale(angles.thirdAngle, -MAX_SCALE_ANGLE, MAX_SCALE_ANGLE, -SCALED_NUM, SCALED_NUM), -1, 1));
             MY_DISTANCE+=ACC()[1];
+            telemetry.addLine()
+                    .addData("distance", MY_DISTANCE);
+            telemetry.addLine()
+                    .addData("acc", ACC()[1]);
+            telemetry.update();
         }
         fld.setPower(0);
         frd.setPower(0);
