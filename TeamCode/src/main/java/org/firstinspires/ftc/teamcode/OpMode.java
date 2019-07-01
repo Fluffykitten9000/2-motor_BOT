90cdacc6567aa4f3217b1cb80f435fb82890e018
Fluffykitten9000 <50753141+Fluffykitten9000@users.noreply.github.com>
2019-07-01 19:14:38 -0400

    Update OpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
@@ -1,31 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 package org.firstinspires.ftc.teamcode;
 
@@ -41,16 +13,20 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@TeleOp(name="OP OF EPICNESSSSSS", group="EPIC STUFF")
+@TeleOp(name="OP OF EPIC", group="EPIC STUFF")
 public class OpMode extends com.qualcomm.robotcore.eventloop.opmode.OpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor ld = null;
     private DcMotor rd = null;
 
-    BNO055IMU imu;
+    private  BNO055IMU imu;
 
-    Orientation angles;
+    private Orientation angles;
+
+    private double LAST_IMU_ANGLE = 0;
+
+    double SCALE = 4;
 
     @Override
     public void init() {
@@ -73,8 +49,6 @@ public class OpMode extends com.qualcomm.robotcore.eventloop.opmode.OpMode {
         rd.setDirection(DcMotor.Direction.REVERSE);
 
         telemetry.addData("Status", "Initialized");
-
-        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
     }
 
     @Override
@@ -86,13 +60,19 @@ public class OpMode extends com.qualcomm.robotcore.eventloop.opmode.OpMode {
         runtime.reset();
     }
 
-    private double rightIMUp = angles.firstAngle/2.5;
-    private double leftIMUp = (angles.firstAngle/2.5)*-1;
-
     @Override
     public void loop() {
 
-        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+        if (gamepad1.dpad_up) SCALE+=.01;
+        if (gamepad1.dpad_down) SCALE-=.01;
+
+        double SCALED = 1;
+
+        angles = imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+
+        double IMUp = LAST_IMU_ANGLE - angles.secondAngle;
+
+        LAST_IMU_ANGLE = angles.secondAngle;
 
         double leftDP;
         double rightDP;
@@ -100,8 +80,10 @@ public class OpMode extends com.qualcomm.robotcore.eventloop.opmode.OpMode {
         double leftUP = gamepad1.left_stick_y;
         double rightUP = gamepad1.right_stick_y;
 
-        leftDP = Range.clip(leftUP + leftIMUp, -1.0, 1.0);
-        rightDP = Range.clip(rightUP + rightIMUp, -1.0, 1.0);
+        if (IMUp>-1&&IMUp<0.1) IMUp=0;
+
+        leftDP = Range.clip(leftUP + Range.scale(IMUp, -SCALE, SCALE, -SCALED, SCALED), -1.0, 1.0);
+        rightDP = Range.clip(rightUP - Range.scale(IMUp, -SCALE, SCALE, -SCALED, SCALED), -1.0, 1.0);
 
         ld.setPower(leftDP);
         rd.setPower(rightDP);
@@ -109,6 +91,7 @@ public class OpMode extends com.qualcomm.robotcore.eventloop.opmode.OpMode {
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("leftDP", leftDP);
         telemetry.addData("rightDP", rightDP);
+        telemetry.addData("SCALE", SCALE);
         telemetry.update();
     }
 
