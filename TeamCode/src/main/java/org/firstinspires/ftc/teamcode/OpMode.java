70855d8460ad39658f3834f411ca39a1b6eadc31
Fluffykitten9000 <50753141+Fluffykitten9000@users.noreply.github.com>
2019-07-01 02:01:09 -0400

    Update OpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
@@ -29,11 +29,14 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @TeleOp(name="OP OF EPICNESSSSSS", group="EPIC STUFF")
 public class OpMode extends com.qualcomm.robotcore.eventloop.opmode.OpMode {
@@ -44,53 +47,39 @@ public class OpMode extends com.qualcomm.robotcore.eventloop.opmode.OpMode {
     private DcMotor bld = null;
     private DcMotor brd = null;
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
+    BNO055IMU imu;
+
+    Orientation angles;
+
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
         frd = hardwareMap.get(DcMotor.class, "frd");
         fld = hardwareMap.get(DcMotor.class, "fld");
         brd = hardwareMap.get(DcMotor.class, "brd");
         bld = hardwareMap.get(DcMotor.class, "bld");
 
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
         fld.setDirection(DcMotor.Direction.REVERSE);
         frd.setDirection(DcMotor.Direction.REVERSE);
         bld.setDirection(DcMotor.Direction.FORWARD);
         brd.setDirection(DcMotor.Direction.REVERSE);
 
-        // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
     @Override
     public void init_loop() {
     }
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
     @Override
     public void start() {
         runtime.reset();
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
     @Override
     public void loop() {
-        // Setup a variable for each drive wheel to save power level for telemetry
+        
         double fldP;
         double frdP;
         double bldP;
